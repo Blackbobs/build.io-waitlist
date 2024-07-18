a89f2a6685f442a0b84e388f52a6c64d74a26711
Black Bobs <ayodejiayodele350@gmail.com>
2024-07-18 21:45:41 +0100

    changed the font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+// import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({
-  weight: ["400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-poppins",
-});
+// const poppins = Poppins({
+//   weight: ["400", "500", "600", "700", "800", "900"],
+//   subsets: ["latin"],
+//   display: "swap",
+//   variable: "--font-poppins",
+// });
 export const metadata: Metadata = {
   title: "build.io waitlist",
   description: "Waitlist Page",
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
